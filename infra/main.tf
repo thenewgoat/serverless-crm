6f24288d8d2160d27a77735d5cfd28690ffb5dc9
Xinyang <75788859+thenewgoat@users.noreply.github.com>
2025-09-20 23:01:40 +0800

    Update main.tf
    set integrations to use payload format version 2.0

--- infra/main.tf
+++ infra/main.tf
@@ -121,12 +121,14 @@ resource "aws_apigatewayv2_integration" "clients" {
   api_id           = aws_apigatewayv2_api.crm_api.id
   integration_type = "AWS_PROXY"
   integration_uri  = aws_lambda_function.clients.invoke_arn
+  payload_format_version = "2.0"
 }
 
 resource "aws_apigatewayv2_integration" "accounts" {
   api_id           = aws_apigatewayv2_api.crm_api.id
   integration_type = "AWS_PROXY"
   integration_uri  = aws_lambda_function.accounts.invoke_arn
+  payload_format_version = "2.0"
 }
 
 # =======================
