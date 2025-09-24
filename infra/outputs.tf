12bc6d18928be9d999c9f39f46cc16bb043143bd
Xinyang <75788859+thenewgoat@users.noreply.github.com>
2025-09-24 15:20:46 +0800

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -36,7 +36,7 @@ output "aurora_cluster_arn" {
 
 output "aurora_secret_arn" {
   description = "ARN of the Aurora Secrets Manager secret (for Data API migrations)"
-  value       = aws_secretsmanager_secret.aurora.arn
+  value       = data.aws_secretsmanager_secret.db.arn
 }
 
 output "db_secret_name" {
