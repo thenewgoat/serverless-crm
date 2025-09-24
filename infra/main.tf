b63a32f709f89823f43fbf54e75f30f401da916e
Xinyang <zhangxinyang888@gmail.com>
2025-09-24 10:12:31 +0800

    Update main.tf
    give lambda vpc access execution role

--- infra/main.tf
+++ infra/main.tf
@@ -273,6 +273,11 @@ resource "aws_iam_role_policy_attachment" "lambda_rds_connect_attach" {
   policy_arn = aws_iam_policy.lambda_rds_connect.arn
 }
 
+resource "aws_iam_role_policy_attachment" "lambda_vpc_access" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
 #######################################
 # Lambda Functions
 #######################################
